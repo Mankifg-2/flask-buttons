c830d87a20381d122183150d6c1d6bfc62482c4b
Mankifg-2 <jaka.horvat@ospuconci.si>
2022-10-25 13:47:40 +0200

    shorter code/possible test

--- templates/buttons.js
+++ templates/buttons.js
@@ -3,94 +3,53 @@ var lbState = "Off";
 var lgState = "Off";
 
 
-            function yLight(){
+function yLight(){
+    document.getElementById("lSound").play();
+    var yb = document.getElementById("yb");
+    if (lyState == "Off"){
+        console.log("1");
+        yb.src  = "./Buttons/Yellow_Bulb_On.png";
+        lyState = "On";
+    } else if(lyState == "On"){
+        console.log("2");
+        yb.src  = "./Buttons/Yellow_Bulb_Off.png";
+        lyState = "Off";
+    }
+}
 
-                document.getElementById("lSound").play();
+function bLight(){
 
-                var yb = document.getElementById("yb");
+    document.getElementById("lSound").play();
 
-                if (lyState == "Off"){
+    var bb = document.getElementById("bb");
 
-                    console.log("1");
-                    yb.src  = "./Buttons/Yellow_Bulb_On.png";
+    if (lbState == "Off"){
 
-                    lyState = "On";
+        console.log("1");
+        bb.src  = "./Buttons/Blue_Bulb_On.png";
+        lbState = "On";
 
+    } else if(lbState == "On"){
+        console.log("2");
+        bb.src  = "./Buttons/Blue_Bulb_Off.png";
+        lbState = "Off";
+    }
+}
 
-                } else if(lyState == "On"){
+function gLight(){
+    document.getElementById("lSound").play();
+    var gb = document.getElementById("gb");
+    if (lgState == "Off"){
+        console.log("1");
+        gb.src  = "./Buttons/Green_Bulb_On.png";   
+        console.log(gb.src);
+        lgState = "On";
 
-                    console.log("2");
+    } else if(lgState == "On"){
+        
+        console.log("2");
+        gb.src  = "./Buttons/Green_Bulb_Off.png";
+        lgState = "Off";
 
-                    yb.src  = "./Buttons/Yellow_Bulb_Off.png";
-
-                    lyState = "Off";
-
-
-                }
-               
-
-            }
-
-
-            function bLight(){
-
-                document.getElementById("lSound").play();
-
-                var bb = document.getElementById("bb");
-
-                if (lbState == "Off"){
-
-                    console.log("1");
-                    bb.src  = "./Buttons/Blue_Bulb_On.png";
-                    
-
-                    lbState = "On";
-
-
-                } else if(lbState == "On"){
-
-                    console.log("2");
-
-                    bb.src  = "./Buttons/Blue_Bulb_Off.png";
-
-                    lbState = "Off";
-
-
-                }
-           
-
-            }
-
-
-
-
-
-            function gLight(){
-
-                document.getElementById("lSound").play();
-
-                var gb = document.getElementById("gb");
-
-                if (lgState == "Off"){
-
-                    console.log("1");
-                    gb.src  = "./Buttons/Green_Bulb_On.png";
-                    
-                    console.log(gb.src);
-
-                    lgState = "On";
-
-
-                } else if(lgState == "On"){
-
-                    console.log("2");
-
-                    gb.src  = "./Buttons/Green_Bulb_Off.png";
-
-                    lgState = "Off";
-
-
-                }
-               
-
-            }
+    }
+}
